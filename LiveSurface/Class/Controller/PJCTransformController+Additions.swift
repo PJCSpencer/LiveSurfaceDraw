0478827442d68f1313629e3c5178f5bee4c40a03
Peter JC Spencer <pjc.spencer@gmail.com>
2020-08-04 14:54:03 +0100

    Complete tansform set supported, extended cggeometry support

--- LiveSurface/Class/Controller/PJCTransformController+Additions.swift
+++ LiveSurface/Class/Controller/PJCTransformController+Additions.swift
@@ -20,6 +20,8 @@ class PJCTransform2D
         .rightMiddle    : PJCTransforn2DScale.sizedWidth,
         .bottomMiddle   : PJCTransforn2DScale.sizedHeight,
         
+        .topLeft        : PJCTransforn2DScale.sizedTopLeft,
+        .bottomLeft     : PJCTransforn2DScale.sizedBottomLeft,
         .topRight       : PJCTransforn2DScale.sizedTopRight,
         .bottomRight    : PJCTransforn2DScale.sizedBottomRight
     ]
@@ -45,6 +47,19 @@ class PJCTransforn2DScale
         return PJCGeometry(pt, size: size)
     }
     
+    static func sizedWidth(_ geometry: PJCGeometry,
+                          _ dragLocation: CGPoint) -> PJCGeometry
+    {
+        let sized = PJCTransforn2DScale.sizedBottomRight(geometry, dragLocation)
+        let size = CGSize(width: sized.size.width,
+                          height: geometry.size.height)
+         
+        return PJCGeometry(geometry.origin, size: size)
+    }
+    
+    
+    // MARK: - Lateral Vertically
+    
     static func sizedY(_ geometry: PJCGeometry,
                              _ dragLocation: CGPoint) -> PJCGeometry
     {
@@ -61,19 +76,6 @@ class PJCTransforn2DScale
         return PJCGeometry(pt, size: size)
     }
     
-    
-    // MARK: - Lateral Vertically
-    
-    static func sizedWidth(_ geometry: PJCGeometry,
-                          _ dragLocation: CGPoint) -> PJCGeometry
-    {
-        let sized = PJCTransforn2DScale.sizedBottomRight(geometry, dragLocation)
-        let size = CGSize(width: sized.size.width,
-                          height: geometry.size.height)
-         
-        return PJCGeometry(geometry.origin, size: size)
-    }
-    
     static func sizedHeight(_ geometry: PJCGeometry,
                            _ dragLocation: CGPoint) -> PJCGeometry
     {
@@ -87,15 +89,6 @@ class PJCTransforn2DScale
     
     // MARK: - Corner(s)
     
-    static func sizedBottomRight(_ geometry: PJCGeometry,
-                                 _ dragLocation: CGPoint) -> PJCGeometry
-    {
-        let offset = dragLocation - (geometry.origin + geometry.size)
-        let size = (geometry.size + offset).greaterThan(PJCSelectionView.minimumCombinedRadius)
-        
-        return PJCGeometry(geometry.origin, size: size)
-    }
-    
     static func sizedTopRight(_ geometry: PJCGeometry,
                               _ dragLocation: CGPoint) -> PJCGeometry
     {
@@ -105,6 +98,44 @@ class PJCTransforn2DScale
         
         if size.height <= PJCSelectionView.minimumCombinedRadius
         { pt.y = geometry.origin.y + geometry.size.height - PJCSelectionView.minimumCombinedRadius }
+        
+        return PJCGeometry(pt, size: size)
+    }
+    
+    static func sizedBottomRight(_ geometry: PJCGeometry,
+                                 _ dragLocation: CGPoint) -> PJCGeometry
+    {
+        let offset = dragLocation - (geometry.origin + geometry.size)
+        let size = (geometry.size + offset).greaterThan(PJCSelectionView.minimumCombinedRadius)
+        
+        return PJCGeometry(geometry.origin, size: size)
+    }
+    
+    static func sizedTopLeft(_ geometry: PJCGeometry,
+                             _ dragLocation: CGPoint) -> PJCGeometry
+    {
+        let offset = dragLocation - geometry.origin
+        var pt = geometry.origin + offset
+        let size = (geometry.size - offset).greaterThan(PJCSelectionView.minimumCombinedRadius)
+        
+        if size.height <= PJCSelectionView.minimumCombinedRadius
+        { pt.y = geometry.origin.y + geometry.size.height - PJCSelectionView.minimumCombinedRadius }
+        
+        if size.width <= PJCSelectionView.minimumCombinedRadius
+        {  pt.x = geometry.origin.x + geometry.size.width - PJCSelectionView.minimumCombinedRadius }
+        
+        return PJCGeometry(pt, size: size)
+    }
+    
+    static func sizedBottomLeft(_ geometry: PJCGeometry,
+                                _ dragLocation: CGPoint) -> PJCGeometry
+    {
+        let offset = dragLocation - (geometry.origin + (geometry.size * CGSize.unitHeight))
+        var pt = geometry.origin + (offset * CGPoint.unitX)
+        let size = (geometry.size + (offset * CGPoint.unitXNegative)).greaterThan(PJCSelectionView.minimumCombinedRadius)
+        
+        if size.width <= PJCSelectionView.minimumCombinedRadius
+        {  pt.x = geometry.origin.x + geometry.size.width - PJCSelectionView.minimumCombinedRadius }
         
         return PJCGeometry(pt, size: size)
     }
--- LiveSurface/Extension/UIKit+Additions.swift
+++ LiveSurface/Extension/UIKit+Additions.swift
@@ -25,15 +25,47 @@ extension CGRect
 {
     var center: CGPoint
     { return CGPoint(x: midX, y: midY) }
+    
+    var concatenated: CGPoint
+    { return (self.origin + self.size) }
+    
+    func inset(by value: CGFloat) -> CGRect
+    {
+        return self.inset(by: UIEdgeInsets(top: 0,
+                                           left: 0,
+                                           bottom: value,
+                                           right: value))
+    }
 }
 
 extension CGPoint
 {
     // MARK: - Constant(s)
     
+    static let unit: CGPoint = CGPoint(x: 1, y: 1)
+    
+    static let unitX: CGPoint = CGPoint(x: 1, y: 0)
+    
+    static let unitXNegative: CGPoint = CGPoint(x: -1, y: 1)
+    
     static let unitY: CGPoint = CGPoint(x: 0, y: 1)
     
     static let unitYNegative: CGPoint = CGPoint(x: 1, y: -1)
+    
+    
+    // MARK: - Conversion
+    
+    var size: CGSize
+    { return CGSize(width: self.x, height: self.y) }
+    
+    
+    // MARK: - Containment
+    
+    func greaterThan(_ value: CGFloat) -> CGPoint
+    {
+        return CGPoint(x: self.x < value ? value : self.x,
+                       y: self.y < value ? value : self.y)
+    }
     
     
     // MARK: - Operator(s)
@@ -49,6 +81,10 @@ extension CGPoint
     static func - (lhs: CGPoint,
                    rhs: CGPoint) -> CGPoint
     { return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y) }
+    
+    static func - (lhs: CGPoint,
+                   rhs: CGFloat) -> CGPoint
+    { return CGPoint(x: lhs.x - rhs, y: lhs.y - rhs) }
     
     static func * (lhs: CGPoint,
                    rhs: CGPoint) -> CGPoint
@@ -67,21 +103,38 @@ extension CGSize
     
     static let unitWidth: CGSize = CGSize(width: 1, height: 0)
     
+    static let unitWidthNegative: CGSize = CGSize(width: -1, height: 1)
+    
     static let unitHeight: CGSize = CGSize(width: 0, height: 1)
+    
+    static let unitHeightNegative: CGSize = CGSize(width: 1, height: -1)
+    
+    
+    // MARK: - Conversion
+    
+    var point: CGPoint
+    { return CGPoint(x: self.width, y: self.height) }
+    
+    
+    // MARK: - Comparison
+    
+    static func <= (_ lhs: CGSize,
+                    rhs: CGFloat) -> Bool
+    { return lhs.width <= rhs || lhs.height <= rhs }
     
     
     // MARK: - Containment
     
-    func lessThan(_ limit: CGFloat) -> CGSize
+    func lessThan(_ value: CGFloat) -> CGSize
     {
-        return CGSize(width: self.width > limit ? limit : self.width,
-                      height: self.height > limit ? limit : self.height)
+        return CGSize(width: self.width > value ? value : self.width,
+                      height: self.height > value ? value : self.height)
     }
     
-    func greaterThan(_ limit: CGFloat) -> CGSize // TODO:Support custom operator, which would communicate more ..?
+    func greaterThan(_ value: CGFloat) -> CGSize // TODO:Support custom operator, which would communicate more ..?
     {
-        return CGSize(width: self.width < limit ? limit : self.width,
-                      height: self.height < limit ? limit : self.height)
+        return CGSize(width: self.width < value ? value : self.width,
+                      height: self.height < value ? value : self.height)
     }
     
     
@@ -94,6 +147,10 @@ extension CGSize
     static func - (lhs: CGSize,
                    rhs: CGPoint) -> CGSize
     { return CGSize(width: lhs.width - rhs.x, height: lhs.height - rhs.y) }
+    
+    static func - (lhs: CGSize,
+                   rhs: CGFloat) -> CGSize
+    { return CGSize(width: lhs.width - rhs, height: lhs.height - rhs) }
     
     static func * (lhs: CGSize,
                    rhs: CGSize) -> CGSize
